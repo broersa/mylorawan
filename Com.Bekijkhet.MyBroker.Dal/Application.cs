08e68b5f47be9fcd706c6026a662cabd800be97f
broersa <broersa@gmail.com>
2016-03-21 13:59:18 +0100

    Latest

--- Com.Bekijkhet.MyBroker.Dal/Application.cs
+++ Com.Bekijkhet.MyBroker.Dal/Application.cs
@@ -4,9 +4,9 @@ namespace Com.Bekijkhet.MyBroker.Dal
 {
 	public class Application
 	{
-		public long Id {get;set;}
-		public string Name {get;set;}
-
+		public long Id { get; set; }
+		public string Name { get; set; }
+		public string AppEUI { get; set; }
 	}
 }
 
---
+++ Com.Bekijkhet.MyBroker.Dal/Session.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Com.Bekijkhet.MyBroker.Dal
+{
+	public class Session
+	{
+		public long Id {get;set;}
+		public long Device { get; set; }
+		public long NwkAddr {get;set;}
+		public string DevNonce {get;set;}
+		public string AppNonce {get;set;}
+		public string NwkSKey {get;set;}
+		public string AppSKey {get;set;}
+		public DateTime Active {get;set;}
+	}
+}
+
